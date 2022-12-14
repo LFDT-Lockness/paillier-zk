9da24a4b636e9cb8eeae198f700ad0af699097f7
d86leader <nikita@dfns.co>
2022-12-14 18:51:23 +0100

    Build curve only for tests
    Signed-off-by: d86leader <nikita@dfns.co>

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,12 @@
 mod common;
-mod curve;
 pub mod group_element_vs_paillier_encryption_in_range;
 pub mod paillier_affine_operation_in_range;
 pub mod paillier_blum_modulus;
 pub mod paillier_decryption_modulo_q;
 pub mod paillier_encryption_in_range;
+
+#[cfg(test)]
+mod curve;
 
 /// Underlying paillier library for which the proofs are made. Use this to get
 /// the correct version of the library
